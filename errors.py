df458f82ca417be7b36540b79d4c180c686404ae
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-09 18:47:02 +0300

    Added errors:TodoNotFound exception and the exception handler

--- errors.py
+++ errors.py
@@ -1,7 +1,19 @@
-# server/errors.py
 from pydantic import ValidationError
+
+class TodoNotFound(Exception):
+    """Raised when a todo item is not found"""
+
+    def __init__(self, todo_id: int) -> None:
+        self.todo_id = todo_id
+        super().__init__(f'Todo with id: {todo_id} not found')
+
 
 def register_error_handlers(app):
     @app.errorhandler(ValidationError)
     def handle_validation_error(e):
         return {"errors": e.errors()}, 400
+
+    @app.errorhandler(TodoNotFound)
+    def handle_todo_not_found(e: TodoNotFound):
+        return {"error": str(e)}
+    
