f6f14e206856791d1fadfba193e77c3cf5011f93
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-10 03:40:12 +0300

    Added the test_list_todos fixture

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,6 +1,10 @@
+from faker import Faker
+
 from app import repository
+from app.extensions import db
 from app.models import Todo
 
+fake = Faker()
 
 def test_create_todo(app, new_todo_schema):
     # assert no todos in the DB
@@ -19,3 +23,28 @@ def test_create_todo(app, new_todo_schema):
     assert todo_db.title == new_todo_schema.title
     assert todo_db.description == new_todo_schema.description
     assert todo_db.completed == new_todo_schema.completed
+
+def test_list_todos():
+    # assert no todos in the database
+    assert Todo.query.count() == 0
+
+    # an empty db must return an empty list.
+    result = repository.list_todos()
+    assert len(result) == 0
+
+    # Add a Todo
+    db_todo = Todo(
+        title=fake.sentence(),
+        description=fake.paragraph(),
+        completed=True
+    )
+    db.session.add(db_todo)
+    db.session.commit()
+
+    # assert the listed todo matches the one added directly to the db
+    result = repository.list_todos()
+    assert len(result) == 1
+    assert result[0]['id'] == db_todo.id
+    assert result[0]['title'] == db_todo.title
+    assert result[0]['description'] == db_todo.description
+    assert result[0]['completed'] == db_todo.completed
