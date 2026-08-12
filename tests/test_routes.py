8077cc46d9455806abbd5cd7a9f1b37d9d901cde
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-12 20:27:22 +0300

    Added the test test_update_todo

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -17,3 +17,18 @@ def test_create_todo(client):
         'description': payload['description'],
         'completed': payload['completed'],
     }
+
+def test_update_todo(client, todo):
+    payload = {
+        'title': fake.sentence(nb_words=2),
+        'description': fake.paragraph(nb_sentences=2),
+        'completed': fake.boolean(),
+    }
+    response = client.put(f'/api/todos/{todo.id}', json=payload)
+    assert response.status_code == 200
+    assert response.json == {
+        'id': todo.id,
+        'title': payload['title'],
+        'description': payload['description'],
+        'completed': payload['completed'],
+    }
