d3bdd6cd77a48cd5a00f651caef51df29b56e01b
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2026-08-12 20:30:43 +0300

    Added the test test_delete_todo

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -40,3 +40,7 @@ def test_patch_todo(client, todo):
     response = client.patch(f'/api/todos/{todo.id}', json=payload)
     assert response.status_code == 200
     assert response.json['title'] == payload['title']
+
+def test_delete_todo(client, todo):
+    response = client.delete(f'/api/todos/{todo.id}')
+    assert response.status_code == 204
